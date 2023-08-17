4db76789880632831bb5c9ceeca844da923b55f1
Wendubeauty <chinwenduabia1@gmail.com>
2023-08-17 09:46:41 -0700

    task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,4 +2,4 @@ def square_matrix_simple(matrix=[]):
     squared_matrix = matrix.copy()
     for i in range(len(matrix)):
         squared_matrix[i] = list(map(lambda x: x**2, matrix[i]))
-        return (squared_matrix)
+    return (squared_matrix)
