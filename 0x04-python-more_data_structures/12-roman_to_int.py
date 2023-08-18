f3adf1e1ce1e6b2f51523866777c8840d75dc978
Wendubeauty <chinwenduabia1@gmail.com>
2023-08-18 04:01:46 -0700

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -7,6 +7,7 @@ def to_subtract(list_num):
             total_to_subtract += n
     return (max_number - total_to_subtract)
 
+
 def roman_to_int(roman_string):
     if not roman_string:
         return 0
@@ -14,7 +15,8 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str):
         return 0
 
-    roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                      'C': 100, 'D': 500, 'M': 1000}
     _list = list(roman_numerals.keys())
     number = 0
     last_numeral_value = 0
