65c80234429eb75cab9d4148f7985e4f8b691853
Wendubeauty <chinwenduabia1@gmail.com>
2023-08-28 23:04:57 -0700

    corrections

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             ind += 1
-        except:
+        except index_error:
             break
     print()
     return(ind)
