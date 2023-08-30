c8aaa217a5c3bc718fa53e432c4c576b57fad0f0
Wendubeauty <chinwenduabia1@gmail.com>
2023-08-30 08:59:44 -0700

    corrections

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,33 +1,32 @@
 #!/usr/bin/python3
-"""Square Class
+"""
+Square Class
 
-This script defines a class for representing a 2D square
-
+This script defines a class for representing a 2D square.
 """
 
-
 class Square:
-    """A 2d square
-
-    This class contains methods that allow manipulation.
-
+    """
+    A 2D square class that contains methods for manipulation.
     """
 
-    def __init__(self, size = 0):
-        """Initialize a square with a given size
+    def __init__(self, size=0):
+        """
+        Initialize a square with a given size.
 
         The size of the new square is private.
 
         Args:
-            size (int): the length of the sides of the square.
+            size (int): The length of the sides of the square.
+        
         Raises:
-            TypeError: size is not an integer
-            ValueError: size is negative
-
+            TypeError: If size is not an integer.
+            ValueError: If size is negative.
         """
 
-        if type(size) is not int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
