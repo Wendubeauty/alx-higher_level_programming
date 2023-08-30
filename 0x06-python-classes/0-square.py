09affceee7b9ca5cc45634fb056c63152edc89b2
Wendubeauty <chinwenduabia1@gmail.com>
2023-08-30 08:18:57 -0700

    task 0

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Square Class
+
+This script defines a class for representing a 2D square
+
+"""
+
+
+class Square:
+    """A 2d square
+
+    This class contains methods that allow manipulation.
+
+    """
+    pass
